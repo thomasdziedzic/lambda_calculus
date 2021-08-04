662bcff86f9e5ee5a43a34b286971d3a8b72fffb
Thomas Dziedzic <thomasdziedzic0@gmail.com>
2021-08-04 06:33:22 -0500

    checkpoint before debugging rust crash

--- src/parser.rs
+++ src/parser.rs
@@ -1,6 +1,7 @@
 use antlr_rust::rule_context::CustomRuleContext;
 use antlr_rust::{InputStream, common_token_stream::CommonTokenStream, tree::ParseTreeVisitor};
 use antlr_rust::tree::Visitable;
+use nom::bitvec::view::AsBits;
 use crate::{LambdaCalculusLexer, LambdaCalculusParser, LambdaCalculusParserContextType, LambdaCalculusVisitor};
 
 use crate::lambdacalculusparser::*;
@@ -15,7 +16,7 @@ pub enum AST<'a> {
 
 pub struct MyLambdaCalculusParser<'i> {
     #[allow(dead_code)]
-    _inputs: Vec<&'i str>,
+    _inputs: Vec<AST<'i>>,
     pub(crate) ast: Option<AST<'i>>
 }
 
@@ -32,56 +33,79 @@ fn str_to_ast<'input>(input: &str) -> Option<AST> {
 
     result.accept(&mut terms);
 
-    terms.ast
+    terms._inputs.pop()
 }
 
 impl<'i> ParseTreeVisitor<'i, LambdaCalculusParserContextType> for MyLambdaCalculusParser<'i> {}
 
 impl<'i> LambdaCalculusVisitor<'i> for MyLambdaCalculusParser<'i> {
-    /*
-    fn visit_term(&mut self, ctx: &crate::TermContext<'i>) {
-        let left = ctx.
-        println!("{}", ctx.get_rule_index());
+    fn visit_parens(&mut self, ctx: &ParensContext<'i>) {
+        self.visit_children(ctx);
     }
-    */
-    /*
-    fn visit_parens(&mut self, ctx: &crate::ParensContext<'i>) {
-        let m_term = ctx.term();
-        match m_term {
-            None => None,
-            Some(term) => term.
-        }
-    }
-    */
-}
 
-/*
-pub fn parse(input: &str) -> IResult<&str, AST> {
-  ast(input)
+    fn visit_application(&mut self, ctx: &ApplicationContext<'i>) {
+        ctx.left.as_ref().unwrap().accept(self);
+        ctx.right.as_ref().unwrap().accept(self);
+
+        let right_val = self._inputs.pop().unwrap();
+        let left_val = self._inputs.pop().unwrap();
+
+        let app = AST::App(Box::new(left_val), Box::new(right_val));
+        self._inputs.push(app);
+    }
+
+    fn visit_abstraction(&mut self, ctx: &AbstractionContext<'i>) {
+        ctx.abs_body.as_ref().unwrap().accept(self);
+
+        let vars = vec![];
+        for variable in ctx.variables {
+            vars.push(variable.text.as_ref());
+        }
+
+        let body = self._inputs.pop().unwrap();
+        let abs = AST::Abs(vars, Box::new(body));
+        self._inputs.push(abs);
+    }
+
+    fn visit_variable(&mut self, ctx: &VariableContext<'i>) {
+        let var_str = ctx.variable.unwrap().text.as_ref();
+        let var = AST::Var(var_str);
+        self._inputs.push(var);
+    }
+
+    fn visit_let(&mut self, ctx: &LetContext<'i>) {
+        ctx.assignment.as_ref().unwrap().accept(self);
+        ctx.body.as_ref().unwrap().accept(self);
+
+        let body = self._inputs.pop().unwrap();
+        let assignment = self._inputs.pop().unwrap();
+        let var_str = ctx.variable.unwrap().text.as_ref();
+        let let_statement = AST::Let(var_str, Box::new(body), Box::new(assignment));
+        self._inputs.push(let_statement);
+    }
 }
-*/
 
 pub fn parse(input: &str) -> Result<AST, &str> {
-    Ok(AST::Var("x"))
+    match str_to_ast(input) {
+        None => Err("Did not get anything back from the parser"),
+        Some(ast) => Ok(ast)
+    }
 }
 
-/*
 #[cfg(test)]
 mod tests {
     use super::*;
 
     #[test]
     fn it_parses_abstraction() {
-        let (input, result) = parse("(λx.x)").unwrap();
-        assert_eq!(input, "");
+        let result = parse("(λx.x)").unwrap();
         let var_x = Box::new(AST::Var("x"));
         assert_eq!(result, AST::Abs(vec!["x"], var_x));
     }
 
     #[test]
     fn it_parses_application() {
-        let (input, result) = parse("(x y)").unwrap();
-        assert_eq!(input, "");
+        let result = parse("(x y)").unwrap();
         let var_x = Box::new(AST::Var("x"));
         let var_y = Box::new(AST::Var("y"));
         assert_eq!(result, AST::App(var_x, var_y));
@@ -89,15 +113,13 @@ mod tests {
 
     #[test]
     fn it_parses_variables() {
-        let (input, result) = parse("x").unwrap();
-        assert_eq!(input, "");
+        let result = parse("x").unwrap();
         assert_eq!(result, AST::Var("x"));
     }
 
     #[test]
     fn it_parses_variables_abstraction_application() {
-        let (input, result) = parse("((λx.x) y)").unwrap();
-        assert_eq!(input, "");
+        let result = parse("((λx.x) y)").unwrap();
         let var_x = Box::new(AST::Var("x"));
         let var_y = Box::new(AST::Var("y"));
         assert_eq!(result, AST::App(Box::new(AST::Abs(vec!["x"], var_x)), var_y));
@@ -105,8 +127,7 @@ mod tests {
 
     #[test]
     fn it_parses_left_application() {
-      let (input, result) = parse("((x y) z)").unwrap();
-      assert_eq!(input, "");
+      let result = parse("((x y) z)").unwrap();
       let var_x = Box::new(AST::Var("x"));
       let var_y = Box::new(AST::Var("y"));
       let var_z = Box::new(AST::Var("z"));
@@ -115,8 +136,7 @@ mod tests {
 
     #[test]
     fn it_parses_right_application() {
-      let (input, result) = parse("(x (y z))").unwrap();
-      assert_eq!(input, "");
+      let result = parse("(x (y z))").unwrap();
       let var_x = Box::new(AST::Var("x"));
       let var_y = Box::new(AST::Var("y"));
       let var_z = Box::new(AST::Var("z"));
@@ -125,8 +145,7 @@ mod tests {
 
     #[test]
     fn it_parses_let() {
-        let (input, result) = parse("let I = (λx.x) in (I a)").unwrap();
-        assert_eq!(input, "");
+        let result = parse("let I = (λx.x) in (I a)").unwrap();
         let var_x = Box::new(AST::Var("x"));
         let var_i = Box::new(AST::Var("I"));
         let var_a= Box::new(AST::Var("a"));
@@ -135,19 +154,16 @@ mod tests {
 
     #[test] #[ignore]
     fn it_associates_abstractions_to_the_right() {
-        let (input, result) = parse("λx.λy.x").unwrap();
-        assert_eq!(input, "");
+        let result = parse("λx.λy.x").unwrap();
         assert_eq!(result, AST::Abs(vec!["x"], Box::new(AST::Abs(vec!["y"], Box::new(AST::Var("x"))))))
     }
 
     #[test] #[ignore]
     fn it_associates_applications_to_the_left() {
-        let (input, result) = parse("a b c").unwrap();
-        assert_eq!(input, "");
+        let result = parse("a b c").unwrap();
         let a = Box::new(AST::Var("a"));
         let b = Box::new(AST::Var("b"));
         let c = Box::new(AST::Var("c"));
         assert_eq!(result, AST::App(Box::new(AST::App(a, b)), c));
     }
 }
-*/
